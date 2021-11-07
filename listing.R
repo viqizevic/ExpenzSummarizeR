6a11742fe87a2b982a78ad9e0fb20af2063e2566
Vicky Tanzil <vicky.tanzil@gmail.com>
2021-11-07 21:08:59 +0100

    Create draft listing

---
+++ listing.R
@@ -0,0 +1,14 @@
+# Create Listing
+
+createListing <- function(datain, yr="all") {
+  ds <- datain
+  if (yr != "all") ds <- filter(ds, year == yr)
+  ds %>% mutate(
+    payee = if_else(is.na(payee), "", payee),
+    memo = if_else(is.na(memo), "", memo)
+  ) %>% arrange(category, date, payee) %>% 
+    select(category, date, payee, account, value, memo) %>% 
+    formattable(list('value' = improvement_formatter))
+}
+
+createList(datain = tb)
