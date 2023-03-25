431aa8da4d3b71f4de370dac8ad78d4fa5528589
viqizevic <vicky.tanzil@gmail.com>
2023-03-25 15:37:47 +0100

    Reduce lengthy lines

--- init.R
+++ init.R
@@ -9,36 +9,40 @@ library(formattable)
 library(lubridate)
 
 # Set a few color variables to get more visually appealing
-customGreen = "#71CA97"
-customRed = "#ff7f7f"
+custom_green <- "#71CA97"
+custom_red <- "#ff7f7f"
 
 # Format improvement
 improvement_formatter <- formatter("span", style = x ~ style(
-  color = ifelse(x > 0, customGreen,
-                  ifelse(x < 0, customRed, "black")))
+  color = ifelse(x > 0, custom_green,
+                  ifelse(x < 0, custom_red, "black")))
 )
 
 categories <- read_csv("categories.csv", show_col_types = FALSE)
 
 # Check function for valid income and expense values
-checkIncomeAndExpense <- function(datain) {
+check_income_and_expense <- function(datain) {
   ds <- datain %>% mutate(
     navalue = (is.na(income) | is.na(expense)),
     inexpvalue = (income != 0 & expense != 0),
     wrongincome = (income < 0),
     wrongexpense = (expense > 0)
   )
-  if(any(ds$navalue)) warning("Unexpected missing income or expense.", immediate. = TRUE)
-  if(any(ds$inexpvalue)) warning("Unexpected both income and expense value given.", immediate. = TRUE)
-  if(any(ds$wrongincome)) warning("Unexpected income < 0.", immediate. = TRUE)
-  if(any(ds$wrongexpense)) warning("Unexpected expense > 0.", immediate. = TRUE)
+  if (any(ds$navalue))
+    warning("Unexpected missing income or expense", immediate. = TRUE)
+  if (any(ds$inexpvalue))
+    warning("Unexpected both income and expense value given", immediate. = TRUE)
+  if (any(ds$wrongincome))
+    warning("Unexpected income < 0", immediate. = TRUE)
+  if (any(ds$wrongexpense))
+    warning("Unexpected expense > 0", immediate. = TRUE)
   ds %>% filter(navalue | inexpvalue | wrongincome | wrongexpense)
 }
 
 # Reader function
-readTransactionsFile <- function(folder="data", filename, acc,
-                                  dateformat="%d.%m.%Y") {
-  file <- paste(folder, filename, sep="/")
+read_trans_file <- function(folder = "data", file, acc,
+                                  dateformat = "%d.%m.%Y") {
+  file <- paste(folder, file, sep="/")
   exl <- read_excel(file) %>% as_tibble %>% clean_names %>%
     left_join(categories, by = "category") %>%
     mutate(
@@ -50,8 +54,11 @@ readTransactionsFile <- function(folder="data", filename, acc,
   if(acc=="Deutsche Bank") {
     exl <- exl %>% mutate(
       payee = ifelse(is.na(payee), umsatzart, payee),
-      memo = ifelse(grepl("Tanzil",payee,ignore.case=TRUE) & (grepl("RINP|Tabungan",memo) | memo=="-"),
-                    paste(memo,iban), memo)
+      memo = ifelse(
+        grepl("Tanzil", payee, ignore.case = TRUE)
+        & (grepl("RINP|Tabungan", memo)
+        | memo == "-"),
+          paste(memo,iban), memo)
     )
   }
   if(acc=="ING DiBa") {
@@ -59,28 +66,29 @@ readTransactionsFile <- function(folder="data", filename, acc,
       payee = ifelse(is.na(payee), buchungstext, payee)
     )
   }
-  if(any(is.na(exl$date))) warning("Failed to get dates. Please check format.", immediate. = TRUE)
-  checkIncomeAndExpense(exl)
+  if(any(is.na(exl$date)))
+    warning("Failed to get dates. Please check format.", immediate. = TRUE)
+  check_income_and_expense(exl)
   exl %>% select(date, payee, memo, income, expense, category) %>%
     mutate(
       account = acc,
       value = income+expense,
-      year_month = format(date, '%Y-%m'),
-      year = format(date, '%Y'),
-      month = format(date, '%B'),
+      year_month = format(date, "%Y-%m"),
+      year = format(date, "%Y"),
+      month = format(date, "%B"),
     )
 }
 
 # Read data
-tb_barclay <- readTransactionsFile(filename="Umsaetze_BC.xlsx", acc="Barclays")
-tb_n26     <- readTransactionsFile(filename="N26-transactions.xlsx", acc="N26", dateformat="%Y-%m-%d")
-tb_lbbamzn <- readTransactionsFile(filename="KKB-Umsaetze.xlsx", acc="LBB Amazon")
-tb_ingdiba <- readTransactionsFile(filename="Umsatzanzeige_ING.xlsx", acc="ING DiBa")
-tb_commrzb <- readTransactionsFile(filename="Umsaetze_CMZB.xlsx", acc="Commerzbank")
-tb_trfwise <- readTransactionsFile(filename="Statement_Wise.xlsx", acc="Wise", dateformat="%d-%m-%Y")
-tb_deutscb <- readTransactionsFile(filename="Kontoumsaetze_DB.xlsx", acc="Deutsche Bank")
-tb_wstnrot <- readTransactionsFile(filename="Umsaetze_WSTR.xlsx", acc="Wüstenrot")
-tb_mintos  <- readTransactionsFile(filename="Mintos-transactions.xlsx", acc="Mintos", dateformat="%Y-%m-%d")
+tb_barclay <- read_trans_file(file = "Umsaetze_BC.xlsx", acc = "Barclays")
+tb_n26     <- read_trans_file(file = "N26-transactions.xlsx", acc = "N26", dateformat="%Y-%m-%d")
+tb_lbbamzn <- read_trans_file(file = "KKB-Umsaetze.xlsx", acc = "LBB Amazon")
+tb_ingdiba <- read_trans_file(file = "Umsatzanzeige_ING.xlsx", acc = "ING DiBa")
+tb_commrzb <- read_trans_file(file = "Umsaetze_CMZB.xlsx", acc = "Commerzbank")
+tb_trfwise <- read_trans_file(file = "Statement_Wise.xlsx", acc = "Wise", dateformat="%d-%m-%Y")
+tb_deutscb <- read_trans_file(file = "Kontoumsaetze_DB.xlsx", acc = "Deutsche Bank")
+tb_wstnrot <- read_trans_file(file = "Umsaetze_WSTR.xlsx", acc = "Wüstenrot")
+tb_mintos  <- read_trans_file(file = "Mintos-transactions.xlsx", acc = "Mintos", dateformat="%Y-%m-%d")
 
 # Combine as one tibble
 tb0 <- rbind(tb_barclay, tb_n26, tb_lbbamzn, tb_ingdiba, tb_commrzb,
