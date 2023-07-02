6b6d031b57e750a44991a3b3068a0e053033f170
viqizevic <vicky.tanzil@gmail.com>
2023-07-02 22:38:03 +0200

    Read commerzbank csv

--- read_folder.R
+++ read_folder.R
@@ -1,9 +1,10 @@
-path <- "/Users/vicky/Documents/celestial/finance/Banking/N26/obsolete"
+#path <- "/Users/vicky/Documents/celestial/finance/Banking/N26/obsolete"
+path <- "/Users/vicky/Documents/celestial/finance/Banking/Commerzbank"
 files <- list.files(path)
-csvfiles <- files[grepl("\\.csv$",files)]
+csvfiles <- files[grepl("\\.csv$",files,ignore.case = TRUE)]
 
 all0 <- Reduce(function(f0, fl) {
-  x <- read.csv(file.path(path,fl)) %>% as_tibble %>% clean_names()
+  x <- read.csv(file.path(path,fl),sep = ";") %>% as_tibble %>% clean_names()
   y <- x %>% distinct()
   if (nrow(x) != nrow(y)) {
     warning("Found duplicate. Please check in csv file ", fl, immediate. = TRUE)
@@ -19,14 +20,21 @@ all <- all0 %>%
   ) %>% 
   select(-category) %>% distinct()
 
-all %>% count(account_number) %>% formattable()
-all %>% count(payment_reference) %>% formattable()
+# For Commerzbank
+all <- all0 %>%
+  mutate(
+    Date = dmy(buchungstag)
+  ) %>% 
+  arrange(Date) %>% 
+  distinct() %>% 
+  select(-Date)
 
-all %>% pull(amount_eur) %>% sum()
 
-write.csv(all, "data/n26.csv")
+#dataout = "data/n26.csv"
+dataout = "data/commerzbank.csv"
+write.csv(all, dataout)
 
-tb_n26c <- read_trans_file(file = "N26-transactions copy.xlsx", acc = "N26c", dateformat="%Y-%m-%d")
+tb_n26c <- read_trans_file(file = "out/N26-transactions copy.xlsx", acc = "N26c", dateformat="%Y-%m-%d")
 
 v <- bind_rows(tb_n26, tb_n26c)
 w <- v %>% count(date, payee) %>% filter(n!=2,n!=4)
