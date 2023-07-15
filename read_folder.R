604286359168c197c077f22158663133f55c31ec
viqizevic <vicky.tanzil@gmail.com>
2023-07-15 11:28:16 +0200

    Create sort_by_date function

--- read_folder.R
+++ read_folder.R
@@ -17,9 +17,6 @@ skiprows <- 1
 skiprows <- 4
 skiprows <- 12
 
-hdr <- TRUE
-hdr <- FALSE
-
 encodingtype <- "latin1"
 encodingtype <- "UTF-8"
 encodingtype <- "unknown"
@@ -31,26 +28,62 @@ read_folder <- function(folder, path, skiprows) {
   if (length(tblfiles)==0) {
     warning("No csv or xlsx file found. Please check folder ", fullpath, immediate. = TRUE)
   } else {
+    # combine all files in folder into one tibble
     all0 <- Reduce(function(f0, fl) {
       if (grepl("csv$", fl, ignore.case = TRUE)) {
         x0 <- read.csv(file.path(fullpath,fl), sep = separator, skip = skiprows,
-                      header = hdr, encoding = encodingtype)
+                      encoding = encodingtype)
       } else {
         x0 <- read_xlsx(file.path(fullpath,fl), skip = skiprows)
       }
       x <- x0  %>% as_tibble %>% clean_names()
+      # check if any duplicate
       y <- x %>% distinct()
       if (nrow(x) != nrow(y)) {
         warning("Found duplicate. Please check in file ", fl, immediate. = TRUE)
       }
+      # if first one then take as it is, else bind with previous one
       if (nrow(f0) == 0) return(x)
       else return(bind_rows(f0, x))
     }, tblfiles, tibble())
-    return(all0 %>% distinct())
+    
+    # remove duplicates
+    all1 <- all0 %>% distinct()
+    return(all1)
   }
 }
 
 all0 <- read_folder(folder, banking_fullpath, skiprows)
+
+sort_by_date <- function(df) {
+  # Save as temporary date for sorting
+  datevars <- c("date", "buchungstag")
+  for (d in datevars) {
+    if (d %in% names(df)) {
+      df["TempDate"] <- df[d]
+    }
+  }
+  if ("TempDate" %in% names(df)) {
+    suppressWarnings({
+      # Format as Date
+      exdt <- pull(df,TempDate)[1]
+      is_dmy <- !is.na(dmy(exdt))
+      is_ymd <- !is.na(ymd(exdt))
+      if (is_dmy) {
+        df <- df %>% mutate(TempDate=dmy(TempDate))
+      } else if (is_ymd) {
+        df <- df %>% mutate(TempDate=ymd(TempDate))
+      } else {
+        warning("Unhandled date format. Please check date format ", exdt, immediate. = TRUE)
+      }
+    })
+    df <- df %>% arrange(TempDate) %>% select(-TempDate)
+  } else {
+    warning("Not date variable found. Please check.", immediate. = TRUE)
+  }
+  return(df)
+}
+all <- sort_by_date(all0)
 
 # For N26
 all <- all0 %>%
@@ -74,6 +107,14 @@ all <- all0 %>%
 all <- all0 %>%
   mutate(
     Date = dmy(transaktionsdatum)
+  ) %>% 
+  arrange(Date) %>% 
+  select(-Date)
+
+# For Barclay
+all <- all0 %>%
+  mutate(
+    Date = dmy(buchungsdatum_2)
   ) %>% 
   arrange(Date) %>% 
   select(-Date)
